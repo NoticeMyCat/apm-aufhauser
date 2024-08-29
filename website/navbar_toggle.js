74bed2c1be34e44ef235c1a061f01c2a2424427c
Do Duy Khanh <khanh.doduy@gmx.at>
2024-08-29 12:46:06 +0200

    version 1

--- website/navbar_toggle.js
+++ website/navbar_toggle.js
@@ -2,16 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbarToggle = document.getElementById("navbar-toggle");
     const navbarClose = document.getElementById("navbar-close");
     const navbarLinks = document.getElementById("navbar-links");
+    const body = document.body; // Zugriff auf das body-Element
 
     navbarToggle.addEventListener("click", function () {
         navbarLinks.classList.add("active");
         navbarToggle.classList.add("hidden");
         navbarClose.classList.add("active");
+        body.style.overflow = "hidden"; // Scrollen verhindern
     });
 
     navbarClose.addEventListener("click", function () {
         navbarLinks.classList.remove("active");
         navbarToggle.classList.remove("hidden");
         navbarClose.classList.remove("active");
+        body.style.overflow = ""; // Scrollen wieder zulassen
     });
 });
