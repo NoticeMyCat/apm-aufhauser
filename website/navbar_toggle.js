b4e716672b1208edc560d5b211467ee427a7fac3
Do Duy Khanh <khanh.doduy@gmx.at>
2024-09-05 11:07:59 +0200

    version 2.6

--- website/navbar_toggle.js
+++ website/navbar_toggle.js
@@ -18,3 +18,31 @@ document.addEventListener("DOMContentLoaded", function () {
         body.style.overflow = ""; // Scrollen wieder zulassen
     });
 });
+
+
+// Funktion zum Setzen der aktiven Klasse basierend auf der aktuellen URL
+function setActiveNavLink() {
+    // Wähle alle Links in der Navbar aus
+    const navLinks = document.querySelectorAll('.navbar-links a');
+
+    // Hole die aktuelle URL
+    const currentURL = window.location.href;
+
+    // Iteriere über die Links und vergleiche den `href` mit der aktuellen URL
+    navLinks.forEach(link => {
+        if (currentURL.includes(link.href)) {
+            // Entferne die `active` Klasse von allen anderen Links
+            navLinks.forEach(link => link.classList.remove('active'));
+
+            // Füge die `active` Klasse dem passenden Link hinzu
+            link.classList.add('active');
+        }
+    });
+}
+
+// Setze die aktive Klasse beim Laden der Seite
+setActiveNavLink();
+
+// Optional: Setze die aktive Klasse auch bei Fenstergrößenänderung oder bei öffnendem Hamburger-Menü
+window.addEventListener('resize', setActiveNavLink);
+
