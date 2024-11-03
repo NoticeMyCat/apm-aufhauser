f4742bacd1687aef0fad8ed1ad9d710fbecf5770
NoticeMyCat <khanh.doduy@gmx.at>
2024-11-03 14:51:34 +0100

    modified the hover.js

--- navbar_hover.js
+++ navbar_hover.js
@@ -2,8 +2,12 @@
 const currentLocation = location.href;
 const menuItem = document.querySelectorAll('.navbar-links a');
 const menuLength = menuItem.length;
+
 for (let i = 0; i < menuLength; i++) {
-    if (menuItem[i].href === currentLocation) {
+    // Setze die `active` Klasse, wenn die URLs übereinstimmen oder der Link die Startseite ist
+    if (menuItem[i].href === currentLocation ||
+        (menuItem[i].href.endsWith('/') && currentLocation.endsWith('/')) ||
+        (menuItem[i].href.endsWith('/index.html') && currentLocation.endsWith('/'))) {
         menuItem[i].classList.add('active');
     }
 }
